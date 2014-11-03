bc45182eaba4552d41d8775754ac5e81a339095c
Maxence Papillon <maxence.papillon@etu.univ-nantes.fr>
2014-11-03 15:19:43 +0100

    Création des classes FIFO Les classes FIFO on étés crées, mais l'implémentation des méthodes reste à faire.

---
+++ src/prog2/concur/exercice2/FIFOBasNiveau.java
@@ -0,0 +1,21 @@
+package prog2.concur.exercice2;
+
+public class FIFOBasNiveau<T> extends AbstractFileBloquanteBornee<T> {
+
+	public FIFOBasNiveau(int n) throws IllegalArgumentException {
+		super(n);
+	}
+
+	@Override
+	public void deposer(T e) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T prendre() throws InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/prog2/concur/exercice2/FIFOHautNiveau.java
@@ -0,0 +1,21 @@
+package prog2.concur.exercice2;
+
+public class FIFOHautNiveau<T> extends AbstractFileBloquanteBornee<T> {
+	
+	public FIFOHautNiveau(int n) throws IllegalArgumentException {
+		super(n);
+	}
+
+	@Override
+	public void deposer(T e) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T prendre() throws InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
